ddc016a694b37665450678de2df69345146b1d49
PapaGeorgeLap <jorgerendongonzalez06@gmail.com>
2025-09-11 19:53:45 -0600

    improve everything

--- script.js
+++ script.js
@@ -1,12 +1,93 @@
 const display = document.getElementById("display");
 const botones = document.querySelectorAll("button");
-const operadores = ["+", "-", "/", "*", "%"]
+const operadores = ["+", "-", "/", "*", "%"];
+
+// Botones
+const igual = document.getElementById("igual");
+const one = document.getElementById("uno");
+const two = document.getElementById("dos");
+const three = document.getElementById("tres");
+const four = document.getElementById("cuatro");
+const five = document.getElementById("cinco");
+const zero = document.getElementById("zero");
+const dot = document.getElementById("punto");
+const add = document.getElementById("sumar");
+const subtract = document.getElementById("restar");
+const six = document.getElementById("seis");
+const seven = document.getElementById("siete");
+const eight = document.getElementById("ocho");
+const nine = document.getElementById("nueve");
+const divide = document.getElementById("dividir");
+const mult = document.getElementById("multiplicar");
+const mod = document.getElementById("modulo");
+const invert = document.getElementById("invertir");
+const del = document.getElementById("AC");
 
 let after = false;
 let first_num = "";
-let current_operator = '';
+let current_operator = "";
 let second_num = "";
-let result = 0;
+let lastResult = 0;
+
+// Function to update display
+function updateDisplay() {
+    display.innerText = first_num + current_operator + second_num;
+}
+
+function result() {
+    lastResult = eval(`${first_num} ${current_operator} ${second_num}`);
+    first_num = lastResult;
+    current_operator = "";
+    second_num = "";
+    after = false;
+    updateDisplay();
+}
+
+// Link keys to
+document.addEventListener("keydown", function(e) {
+    const pressedKey = e.key;
+    if (pressedKey === "Enter"){
+        e.preventDefault();
+        igual.click();
+    } else if (pressedKey === "1"){
+        one.click();
+    } else if (pressedKey === "2"){
+        two.click();
+    } else if (pressedKey === "3"){
+        three.click();
+    } else if (pressedKey === "4"){
+        four.click();
+    } else if (pressedKey === "5"){
+        five.click();
+    } else if (pressedKey === "6"){
+        six.click();
+    } else if (pressedKey === "7"){
+        seven.click();
+    } else if (pressedKey === "8"){
+        eight.click();
+    } else if (pressedKey === "9"){
+        nine.click();
+    } else if (pressedKey === "0"){
+        zero.click();
+    } else if (pressedKey === "+"){
+        add.click();
+    } else if (pressedKey === "-"){
+        subtract.click();
+    } else if (pressedKey === "*"){
+        mult.click();
+    } else if (pressedKey === "/"){
+        e.preventDefault();
+        divide.click();
+    } else if (pressedKey === "%"){
+        mod.click();
+    } else if (pressedKey === "."){
+        dot.click();
+    } else if (pressedKey === "Backspace"){
+        del.click();
+    } else if (pressedKey === "c"){
+        invert.click();
+    }
+});
 
 // Funcionalidad de botones
 botones.forEach(boton => {
@@ -15,39 +96,35 @@ botones.forEach(boton => {
 
         // Numeros
         if (tipo === "numero") {
-            // Si es el primer numero
-            if (display.innerText === "0") {
-                // Remplazar al 0
-                display.innerText = boton.dataset.valor;
-                if (!after){
-                    first_num += boton.dataset.valor;
-                } else {
-                    second_num += boton.dataset.valor;
-                }
+            if (boton.dataset.valor === "." && (first_num.includes(".") && !after || second_num.includes(".") && after)) {
+                return; // Evitar agregar otro punto decimal
+            } 
+            else if (boton.dataset.valor === "0" && first_num === "" && !after) {
+                return; // Evitar agregar ceros a la izquierda en el primer numero
+            }
+            // Si es first_num
+            if (!after) {
+                first_num += boton.dataset.valor;
+                updateDisplay();
+            // Si es second_num
             } else {
-                // Agregar
-                display.innerText += boton.dataset.valor;
-                if (!after){
-                    first_num += boton.dataset.valor;
-                } else {
-                    second_num += boton.dataset.valor;
-                }
-        
+                second_num += boton.dataset.valor;
+                updateDisplay();
             }
 
         // Operadores
         } else if (tipo === "operacion") {
-            let current_text = display.innerText;
-            // Cambiar operador
-            if (operadores.includes(current_text[current_text.length - 1])) {
-                display.innerText = current_text.slice(0, -1) + boton.innerText;
+            // Cambiar operador si no hay segundo numero aun
+            if (first_num !== "" && second_num === "") {
                 current_operator = boton.innerText;
                 after = true;
-            } else {
-                // Agregar
-                display.innerText += boton.innerText;
+                updateDisplay();
+            // Calcular resultado y seguir operando si ya hay segundo numero
+            } else if (first_num !== "" && second_num !== "") {
+                result();
                 current_operator = boton.innerText;
                 after = true;
+                updateDisplay();
             }
 
         // Especiales
@@ -62,30 +139,25 @@ botones.forEach(boton => {
             }
             // Calcular resultado
             if (boton.dataset.accion === "resultado") {
-                let result = eval(`${first_num} ${current_operator} ${second_num}`);
-                display.innerText = result;
-                first_num = result;
-                current_operator = "";
-                second_num = "";
-                after = false;
+                result();
             }
             // Invertir signo
             if (boton.dataset.accion === "invertir") {
-                let current_text = display.innerText;
                 if (!after) {
-                    if (current_text[0] !== "-"){
-                        display.innerText = "-" + current_text;
+                    if (first_num.includes("-")){
+                        first_num = first_num.slice(1);
+                        updateDisplay();
                     } else {
-                        display.innerText = current_text.slice(1);
+                        first_num = "-" + first_num;
+                        updateDisplay();
                     }
                 } else {
-                    let i = current_text.indexOf(second_num);
-                    if (second_num[0] !== "-"){
-                        second_num = "-" + second_num;
-                        display.innerText = first_num + current_operator + second_num;
-                    } else {
+                    if (second_num.includes("-")){
                         second_num = second_num.slice(1);
-                        display.innerText = first_num + current_operator + second_num;
+                        updateDisplay();
+                    } else {
+                        second_num = "-" + second_num;
+                        updateDisplay();
                     }
                 }
             }
